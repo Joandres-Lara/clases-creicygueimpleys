eff0f32a15fe304975f92d5f544a42c1688e7918
Joan Andrés Lara Mora <joandres52752lm@gmail.com>
2021-08-25 23:23:25 -0500

    Loop

---
+++ loops.js
@@ -0,0 +1,82 @@
+// 1 ... 9
+// 1 ... 100
+
+// console.log(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+// 101*50
+// 1050
+// string
+
+// var frase = "Anita lava la tina, en su casa";
+
+// var obj = {
+//  a: "text",
+//  b: {
+//   c: 9
+//  }
+// }
+
+// console.log("A: ", obj.a)
+// console.log("B -> C",obj.b.c);
+
+// console.log("Length: ", frase.length);
+
+// for(var count = 0; count < frase.length; count++){
+//  console.log(count, frase[count]);
+// }
+
+// var suma = 0;
+// var count = 0;
+// var frase = "Anita lava la tina, en su casa";
+// var i; //undefined
+
+// while (count < frase.length) {
+//   // console.log("While: ", count_while);
+//   // if(count_while > 10){
+//   //  break;
+//   // }
+//   // count_while = count_while + 1;
+//   // console.log("While: ", count_while);
+//   // console.log(count, frase[count]);
+//   // count = count + 1;
+// }
+
+//  console.log("Suma: ", suma);
+
+// var count_do_while = 0;
+
+// do{
+//  console.log("Do while: ", count_do_while);
+//  count_do_while++;
+// }while(count_do_while < 10);
+
+// console.log("Count after do while: ", count_do_while);
+
+// for, while, do while
+// !n, !7, !8, !1000
+// 7x6x5x4x3x2x1 
+// function factorial(number, number_2){
+//  if(number === 0){
+//   return 1;
+//  }
+//  return number * factorial(number - 1, number_2) + number_2;
+// }
+
+// (4 * (3) + 2) * (2 + 2) * (1 + 2) * (1);
+
+// console.log("Loop: ", factorial(4, 2));
+// 3*2*1
+// 4*3*2*1 = 24
+// 
+// Programación Orientada a Objetos
+// Carro -> Honda, Shuru, Chevrolet
+//   ->propiedad, (modelo, color, tipo de combustible)
+//   ->metodos, (frenar, acelerar, chocar)
+// -> Abstracción objetos a objetos en programación
+// -> Instancias de los objetos
+// -> Herencia
+// Carro -> Avion {volar, piloto certificado}
+// Personas -> Gerente -> Trabajador
+// Arrays -> Matrices
+// Objectos
+// for, while, do while
+// Patrones de diseño
